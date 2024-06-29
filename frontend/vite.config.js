4cb2a572c157234863b4d7d8af991ef446f947b5
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-29 15:09:25 +0500

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    //   proxy: {
-    //     '/': {
-    //       target: "https://mernbookstorefrontend.vercel.app",
-    //       changeOrigin: true,
-    //       rewrite: (path) => path.replace(/^\/api\/v1/, '')
-    //     }
-    //   }
-    // },
-    plugins: [react()]
-  });
+  // server: {
+  //   proxy: {
+  //     '/': {
+  //       target: "https://mernbookstorefrontend.vercel.app",
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api\/v1/, '')
+  //     }
+  //   }
+  // },
+  plugins: [react()]
+});
