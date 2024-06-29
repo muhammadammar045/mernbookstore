87958edfc06fc3166c74ceef7634f40bd316a5a0
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-29 15:02:49 +0500

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import envVars from './envexport';
 
 export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: `${envVars.backend_uri}`,
+        target: "https://mernbookstorefrontend.vercel.app",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/v1/, '')
       }
