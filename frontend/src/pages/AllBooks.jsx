8c58267a0a756aa88bd572fc06eda96753ca95b3
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-29 14:19:56 +0500

    env changed

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Container from "./Container";
 import BookShop from "../components/BookShop";
+import envVars from "../../envexport";
 
 function AllBooks() {
   return (
     <>
       <Container>
+        {console.log(envVars.backend_uri)}
         <BookShop />
       </Container>
     </>
