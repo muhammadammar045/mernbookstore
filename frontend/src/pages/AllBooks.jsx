16ce9fe1ddba7803298d677cd67def263bc13419
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-29 13:24:03 +0500

    both done

---
+++ frontend/src/pages/AllBooks.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "./Container";
+import BookShop from "../components/BookShop";
+
+function AllBooks() {
+  return (
+    <>
+      <Container>
+        <BookShop />
+      </Container>
+    </>
+  );
+}
+
+export default AllBooks;
