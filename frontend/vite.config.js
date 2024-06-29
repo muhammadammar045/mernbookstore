47819aae16fbf98059888111c0ad2ee0601e070e
Ammari045 <Ammari045@users.noreply.github.com>
2024-06-29 15:08:12 +0500

    changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    proxy: {
-      '/api/v1': {
-        target: "https://mernbookstorefrontend.vercel.app",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, '')
-      }
-    }
-  },
-  plugins: [react()]
-});
+    //   proxy: {
+    //     '/': {
+    //       target: "https://mernbookstorefrontend.vercel.app",
+    //       changeOrigin: true,
+    //       rewrite: (path) => path.replace(/^\/api\/v1/, '')
+    //     }
+    //   }
+    // },
+    plugins: [react()]
+  });
